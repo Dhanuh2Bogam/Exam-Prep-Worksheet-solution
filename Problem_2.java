31e8ca8d49d6792677be8dbc008eb40b2d91dff9
Dhanuh Bogam <154802050+Dhanuh2Bogam@users.noreply.github.com>
2026-03-18 13:43:09 -0700

    In progress almost done(2nd question)

---
+++ Problem_2.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Problem_2{
+public static void main(String[] args){
+int[] a = {3,1,4};
+int[] b = {2.7.1};
+int[] c = appendArrays(a,b);
+System.out.println(Arrays.toString(c));
+}
+
+public static int[] appendArrays(int[] a, int[] b){
+int[] arr = new int[((a.length()-1) + (b.length()-1))];
+for(int x = 0; x < a.length()-1; x++){
+arr[x] = a[x];
+}
+int r = a.length()-1;
+for(int each: b){
+
+
+}
+}
+
+
+
+}
