2d66e7522c750acbd02d907c30ccb7ce04b45c69
Dhanuh Bogam <154802050+Dhanuh2Bogam@users.noreply.github.com>
2026-03-23 22:08:05 -0700

    answer for 3rd in comment

---
+++ Problem_3.java
@@ -0,0 +1,26 @@
+
+
+public class Oof {
+   static boolean frue;
+   final boolean lanif = false;
+   boolean bool;
+   boolean tralse;
+}
+// Client Code
+Oof batman = new Oof();
+Oof robin = new Oof();
+batman.bool = true;
+robin.bool = false;
+batman.frue = true;
+robin.frue = false;
+batman.tralse = true;
+robin.tralse = false;
+
+if (batman.bool && batman.frue && batman.tralse) {
+	System.out.println(“I’m Batman”);
+} else {
+	System.out.println(“Nightwing!”);
+}
+
+
+//nightwing!
